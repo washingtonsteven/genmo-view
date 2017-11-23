f3f5ed776ce69f5e1fe2ea20097ed040d781021c
Steven Washington <washington.steven@gmail.com>
2017-11-23 16:00:36 -0500

    Hookup genmo to FE react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Genmo from './genmo';
+import world_data from './genmo/data/data.json';
+
+class DisplayMessage extends Component {
+  render() {
+    return (
+      <div className="message"><pre>{this.props.message}</pre></div>
+    )
+  }
+}
+
+class ActionBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttons:[
+        'look',
+        'ls',
+        'move n', 
+        'move s',
+        'move e',
+        'move w',
+        'talk'
+      ]
+    }
+  }
+
+  actionButtons() {
+    let buttons = [];
+    for (let i = 0; i < this.state.buttons.length; i++) {
+      buttons.push(<button onClick={() => this.doAction(this.state.buttons[i])} key={i}>{this.state.buttons[i]}</button>);
+    }
+    return buttons;
+  }
+
+  doAction(action) {
+    if (this.props.action && typeof this.props.action === 'function') {
+      this.props.action(action);
+    }
+  }
+
+  render() {
+    return(
+      <div className="actionBar">
+        { this.actionButtons() }
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.genmo = new Genmo({
+      world_data,
+      output:(message, opts) => {
+        // this.state.messages.push(message);
+      }
+    });
+
+    this.state = { genmoState:this.genmo.currentState() };
+  }
+
+  allMessages() {
+    let displayMessages = [];
+    
+    for(let i = 0; i < this.state.genmoState.length(); i++) {
+      const currentFrame = this.state.genmoState.get(i);
+      if (currentFrame.action === 'output')
+        displayMessages.push(<DisplayMessage message={currentFrame.value} key={currentFrame.key} />);
+    }
+
+    return displayMessages;
+  }
+
+  updateState() {
+    this.setState({ genmoState:this.genmo.currentState() });
+  }
+
+  sendCommand(command) {
+    this.genmo.sendCommand(command);
+    this.updateState();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ActionBar action={(a) => this.sendCommand(a)} />
+        <div className="messages">
+          { this.allMessages() }
+        </div>
       </div>
     );
   }
